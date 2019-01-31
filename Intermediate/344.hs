3e3d426c74ae2be1757368dc4cee282f8f81f289
Juri Dispan <juri.dispan@me.com>
2019-02-01 00:39:40 +0100

    Day 344 I

--- Intermediate/344.hs
+++ Intermediate/344.hs
@@ -5,25 +5,40 @@ import Data.List
 import Data.Maybe
 
 -- parsing stuff and main
+type Ressources = (Int, Int, Int)
 
+parseLine :: String -> (Ressources, Ressources)
 parseLine ln = let nums = map read $ words ln in (toTuple (take 3 nums), toTuple (drop 3 nums))
 
+toTuple :: [Int] -> Ressources
 toTuple (a:b:c:_) = (a, b, c)
 
-main :: IO ()
-main = do
-    input <- readFile "input/344.txt"
+runWithInputFile :: String -> IO ()
+runWithInputFile path = do
+    -- read and parse input file
+    input <- readFile path
     let (strInRc:strReqRcs) = lines input
     let initialState = toTuple $ map read $ words strInRc
     let prcs = map (\(n, (x, y)) -> (n, x, y)) $ zip [0..] $ map parseLine strReqRcs
+    
+    -- run banker's algorithm and print result
     let result = case evalState (banker prcs) initialState of
                     Nothing -> "No result"
                     Just x  -> concat $ map (\n -> "p" ++ show n ++ " ") x
     putStrLn result
 
+
+-- Correct output is:
+-- p1 p3 p0 p2 p4 (or some other correct solution)
+-- No result
+main :: IO ()
+main = do
+    runWithInputFile "input/344.txt"
+    runWithInputFile "input/344f.txt"
+
 -- interesting stuff 
    
-type Ressources = (Int, Int, Int)   
+
 
 banker :: [(Int, Ressources, Ressources)] -> State Ressources (Maybe [Int])
 banker [] = return $ Just []
